e682788d17b816e3cef6cf6004a99e724680bfdb
Temesgen Zelalem <temesgenzelalem167@gmail.com>
2026-08-14 23:04:58 +0300

    FIX: Remove potentially missing Redis provider and correct database migration config

--- backend/config/database.php
+++ backend/config/database.php
@@ -34,7 +34,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('DATABASE_URL', env('DB_URL')),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -49,7 +49,10 @@ return [
 
     ],
 
-    'migrations' => 'migrations',
+    'migrations' => [
+        'table' => 'migrations',
+        'update_date_on_publish' => true,
+    ],
 
     'redis' => [
 
