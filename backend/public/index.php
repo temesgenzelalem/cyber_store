2ceab153a9da45a95ccc3a0a5de2ad1a9ababf6f
Temesgen Zelalem <temesgenzelalem167@gmail.com>
2026-08-14 23:57:17 +0300

    DIAG: Success marker in index.php to verify deployment

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,23 +1,3 @@
 <?php
-// VERSION: 3.0.0 (FINAL RESTORE)
-
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-require __DIR__.'/../vendor/autoload.php';
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+echo "DEPLOYMENT SUCCESSFUL - COMMIT 0860623";
+exit;
