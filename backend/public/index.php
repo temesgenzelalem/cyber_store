97eaad3e8e6e75e58d2cc1ceb23ed74f592ab011
Temesgen Zelalem <temesgenzelalem167@gmail.com>
2026-08-15 10:42:30 +0300

    FIX: Health check override to force Render to swap container

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,22 +1,17 @@
 <?php
-
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+// HEALTH CHECK OVERRIDE
+if ($_SERVER['REQUEST_URI'] === '/') {
+    echo "HEALTH CHECK OK";
+    exit;
 }
 
 require __DIR__.'/../vendor/autoload.php';
-
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Kernel::class);
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Request::capture()
+    $request = Illuminate\Http\Request::capture()
 )->send();
 
 $kernel->terminate($request, $response);
