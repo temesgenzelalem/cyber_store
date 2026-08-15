49d370d58ff1c5cc5e5512142e84cb5cd4b8d2bb
Temesgen Zelalem <temesgenzelalem167@gmail.com>
2026-08-15 11:23:32 +0300

    FIX: Global storage permissions and standard config/app.php

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,17 +1,22 @@
 <?php
-// HEALTH CHECK OVERRIDE
-if ($_SERVER['REQUEST_URI'] === '/') {
-    echo "HEALTH CHECK OK";
-    exit;
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
 
 require __DIR__.'/../vendor/autoload.php';
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+    $request = Request::capture()
 )->send();
 
 $kernel->terminate($request, $response);
