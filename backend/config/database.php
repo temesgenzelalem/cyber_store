20ff395674a238d4ca11dedf224d7647fb02108f
Temesgen Zelalem <temesgenzelalem167@gmail.com>
2026-08-15 10:37:19 +0300

    FIX: Set migrations table name as string to fix TypeError in PostgresBuilder

--- backend/config/database.php
+++ backend/config/database.php
@@ -33,10 +33,7 @@ return [
 
     ],
 
-    'migrations' => [
-        'table' => 'migrations',
-        'update_date_on_publish' => true,
-    ],
+    'migrations' => 'migrations',
 
     'redis' => [
         'client' => env('REDIS_CLIENT', 'phpredis'),
